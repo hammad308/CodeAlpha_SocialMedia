d643af1aa883a64733cc01bf6f7d5136434a1312
Muhammad Hammad Ali <mh6139251@gmail.com>
2026-06-09 23:13:28 +0500

    add Post model with schema for content, user, and likesCount

---
+++ models/Post.js
@@ -0,0 +1,23 @@
+const mongoose= require("mongoose");
+
+const postSchema= new mongoose.Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    likesCount:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }
+    ]
+},{
+        timestamps:true
+});
+
+module.exports= mongoose.model("Post",postSchema);
